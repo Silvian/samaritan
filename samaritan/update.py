3418efb74b2ab6d8a228758bd3011fc2a53fd6d7
silvian <silvian.dragan@gmail.com>
2016-07-14 22:42:23 +0100

    Created a script to assist with release process.

---
+++ samaritan/update.py
@@ -0,0 +1,28 @@
+"""
+@author: Silvian Dragan
+@Date: 02/05/2016
+@Copyright: Copyright 2016, Samaritan CMA - Published under GNU General Public Licence v3
+@Details: https://github.com/Silvian/samaritan
+"""
+
+
+import os
+import shutil
+import settings
+settings_sample = __import__('settings-sample')
+STATUS = "Release"
+
+if settings_sample.STATUS == STATUS:
+    replacements = {settings.VERSION: settings_sample.VERSION, settings.STATUS: settings_sample.STATUS}
+
+    with open('settings.py') as infile, open('settings-new.py', 'w') as outfile:
+        for line in infile:
+            for src, target in replacements.iteritems():
+                line = line.replace(src, target)
+            outfile.write(line)
+
+    # cleanup temp file
+    shutil.copyfile("settings-new.py", "settings.py")
+    os.remove("settings-new.py")
+    print("Release performed to version: " + settings.VERSION)
+    exit(1)
