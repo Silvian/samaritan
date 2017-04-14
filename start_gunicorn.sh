4f88b019f76432f904ee2f8d4e7272e50b604727
silvian.dragan@gmail.com <Pytahgoras1989>
2017-04-14 17:05:40 +0100

    Removed local environment settings

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -2,7 +2,6 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-export STATIC_ROOT=/usr/src/app/static/
 python manage.py collectstatic --noinput
 exec gunicorn samaritan.wsgi:application \
     --bind 0.0.0.0:8000 \
