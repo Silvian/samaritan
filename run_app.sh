aa5af54e7c364a557f9ffbc1b32b939f9aa0ac30
silvian <silvian.dragan@gmail.com>
2017-06-11 22:20:55 +0100

    added sleep time for 5 seconds

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 5
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'root@admin.com', 'root')" | python manage.py shell
 python manage.py runserver 0.0.0.0:8000
