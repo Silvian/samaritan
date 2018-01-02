618624f605c8008939dd0bb4f029306d281574f9
silvian <silvian.dragan@gmail.com>
2018-01-02 00:51:38 +0000

    Updated the scripts.

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -3,5 +3,6 @@
 sleep 5
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'root@admin.com', 'root')" | python manage.py shell
+python manage.py createmembershiptypes
+python manage.py createdefaultchurchrole
 python manage.py runserver 0.0.0.0:8000
-
