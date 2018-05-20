d946ff11b5de9939b5d6430d5abd5d64c3d7dc8b
silvian.dragan <silvian.dragan@glintpay.com>
2018-05-20 23:38:20 +0100

    modified memcached default port so it doesn't clash with existing memcached services

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = 'samaritan.wsgi.application'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': '127.0.0.1:12211',
     }
 }
 
