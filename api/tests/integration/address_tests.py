cf2f801f52cb3b6d30db719f281485cc48032b56
AbelHristodor <habelor@gmail.com>
2018-06-26 20:45:25 +0200

    Added AddressModelFactory

--- api/tests/integration/address_tests.py
+++ api/tests/integration/address_tests.py
@@ -1,1 +1,17 @@
 """API integration address tests."""
+import factory
+from django.test import TestCase
+from samaritan.models import Address
+from factory.django import DjangoModelFactory
+
+
+class AddressModelFactory(DjangoModelFactory):
+    """Factory for address"""
+
+    number = factory.Faker('text')
+    street = factory.Faker('text')
+    locality = factory.Faker('text')
+    post_code = factory.Faker('text')
+
+    class Meta:
+        model = Address
