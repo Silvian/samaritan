be51f992a34f4854e2282c89d8018a8140db4d6d
silvian.dragan <silvian.dragan@glintpay.com>
2018-06-26 22:28:04 +0100

    wrote first address integration test example

--- api/tests/integration/__init__.py
+++ api/tests/integration/__init__.py
@@ -1,4 +1,29 @@
 """API integration tests factories."""
 
+import factory
+from django_common.auth_backends import User
 
-"""Place django model factories here for each django model you are testing in the api your factories here."""
+from factory.django import DjangoModelFactory
+from samaritan.models import Address
+
+
+class UserFactory(DjangoModelFactory):
+    """Factory for users."""
+
+    username = factory.Faker('name')
+
+    class Meta:
+        model = User
+
+
+class AddressFactory(DjangoModelFactory):
+    """Factory for address."""
+
+    number = factory.Faker('text')
+    street = factory.Faker('text')
+    locality = factory.Faker('text')
+    city = factory.Faker('text')
+    post_code = factory.Faker('text')
+
+    class Meta:
+        model = Address
--- api/tests/integration/address_tests.py
+++ api/tests/integration/address_tests.py
@@ -1,17 +1,31 @@
 """API integration address tests."""
-import factory
+
 from django.test import TestCase
-from samaritan.models import Address
-from factory.django import DjangoModelFactory
+
+from api.tests.integration import UserFactory, AddressFactory
 
 
-class AddressModelFactory(DjangoModelFactory):
-    """Factory for address"""
+class TestAddressIntegrationTestCase(TestCase):
+    """Test address model integration tests."""
 
-    number = factory.Faker('text')
-    street = factory.Faker('text')
-    locality = factory.Faker('text')
-    post_code = factory.Faker('text')
+    def setUp(self):
+        self.user = UserFactory(is_superuser=True)
+        self.address = AddressFactory()
 
-    class Meta:
-        model = Address
+    def test_listing_addresses(self):
+        """Test that an authenticated user can view all addresses."""
+        self.client.force_login(user=self.user)
+        response = self.client.get("/api/addresses/getAll")
+
+        self.assertEquals(
+            response.status_code,
+            200,
+        )
+
+        response_json = response.json()
+        self.assertEqual(self.address.id, response_json[0]['pk'])
+        self.assertEqual(self.address.number, response_json[0]['fields']['number'])
+        self.assertEqual(self.address.street, response_json[0]['fields']['street'])
+        self.assertEqual(self.address.locality, response_json[0]['fields']['locality'])
+        self.assertEqual(self.address.city, response_json[0]['fields']['city'])
+        self.assertEqual(self.address.post_code, response_json[0]['fields']['post_code'])
