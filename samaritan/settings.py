1ef1fccc5f21bb8624ae2249bc520b26ddca5563
Silvian <silvian.dragan@gmail.com>
2020-11-26 17:43:30 +0000

    updated to tagged release v1.12

--- samaritan/settings.py
+++ samaritan/settings.py
@@ -25,7 +25,7 @@ from celery.schedules import crontab
 AUTHOR = "Silvian Dragan"
 COPYRIGHT = "Copyright {year}, Samaritan CMA (Church Management Assistant)"
 LICENCE = "GPLv3"
-VERSION = "1.11"
+VERSION = "1.12"
 MAINTAINER = "Silvian Dragan"
 EMAIL = "silvian.dragan@gmail.com"
 
